01a64e12591674c520c28d952d1cd1bdb8cd7be4
Otavio Augusto <tavioalves@gmail.com>
2019-08-01 11:16:16 -0300

    refactor and clean: Remove and delete unecessary files, and refactor some code

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    {siteTitle}
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
